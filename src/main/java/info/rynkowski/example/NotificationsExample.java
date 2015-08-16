aa61bf7a1293f18f6aaaace756f2f0f5572e2b12
Grzegorz Rynkowski <grzegorz.rynkowski@gmail.com>
2015-08-16 12:29:47 +0200

    Added NotificationExample.java

---
+++ src/main/java/info/rynkowski/example/NotificationsExample.java
@@ -0,0 +1,12 @@
+package info.rynkowski.example;
+
+import org.freedesktop.Notifications;
+
+public class NotificationsExample {
+
+  private Notifications notifications;
+
+  public static void main(String[] args) {
+    System.out.println("Hallo!");
+  }
+}
